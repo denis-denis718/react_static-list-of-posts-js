70a228ddbbea0e6e15697066b1f99392b5cd2876
Denis <denchick1546450@gmail.com>
2025-09-01 19:08:45 +0300

    add solution

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import postsFromServer from './api/posts.json';
 import commentsFromServer from './api/comments.json';
 import usersFromServer from './api/users.json';
 import { PostList } from './components/PostList/PostList';
-import { CommentList } from './components/CommentList/CommentList';
-import { UserInfo } from './components/UserInfo/UserInfo';
 
 function getUserById(userId) {
   return usersFromServer.find(user => user.id === userId) ?? null;
@@ -19,13 +17,13 @@ const comments = commentsFromServer.map(comment => ({
 const posts = postsFromServer.map(post => ({
   ...post,
   user: getUserById(post.userId),
+  comments: comments.filter(comment => comment.postId === post.id),
 }));
 
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
-    <CommentList comments={comments} />
+
     <PostList posts={posts} />
-    <UserInfo />
   </section>
 );
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,13 +1,21 @@
 import './PostInfo.scss';
+import { UserInfo } from '../UserInfo/UserInfo';
+import { CommentList } from '../CommentList/CommentList';
 
 export const PostInfo = ({ post }) => (
   <div className="PostInfo">
     <div className="PostInfo__header">
       <h3 className="PostInfo__title">{post.title}</h3>
 
-      <p>{' Posted by  '}</p>
+      <p className="PostInfo__author">
+        {' Posted by '}
+        <UserInfo user={post.user} />
+      </p>
     </div>
 
     <p className="PostInfo__body">{post.body}</p>
+
+    {/* Рендерим комментарии к этому посту */}
+    <CommentList comments={post.comments} />
   </div>
 );
